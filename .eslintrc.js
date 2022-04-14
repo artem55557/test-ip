cb18d8a37459f7f8d663849b7f01cc72ddd1f47d
Artem Netesov <netyosov.a@yandex.ru>
2022-04-14 22:53:32 +0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    'comma-dangle': 0,
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 2,
+        },
+        multiline: {
+          max: 2,
+        },
+      },
+    ],
+  },
+}
